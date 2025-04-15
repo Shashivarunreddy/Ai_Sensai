c105f4d8afd4861bbbb56d7c24c7b658b89d1843
Shashivarunreddy <137184492+Shashivarunreddy@users.noreply.github.com>
2025-04-15 10:52:03 +0530

    initialised and added header

---
+++ components/header.jsx
@@ -0,0 +1,52 @@
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs'
+import { Button } from './ui/button'
+import {  ChevronDown, LayoutDashboard, StarsIcon } from 'lucide-react'
+import Link from 'next/link'
+import Image from 'next/image'
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from './ui/dropdown-menu'
+
+const Header = () => {
+  return (
+    <header className='fixed top-0 w-full border-b bg-background/80 backdrop-blur-md z-50 supports-[backdrop-filter]:bg-background/60'>
+      <nav className='container mx-auto px-4 h-16 flex items-center justify-between'>
+
+        <Link href="/">
+        <Image src="/logo.png" alt="Ai sensai " width={200} height={60}  className="h-12 py-1 w-auto object-contain"/>
+        </Link>
+
+        <div className='flex items-center space-x-2 md:space-x-4'>
+          <SignedIn>
+            <Link href={"/dashboard"}>
+            <Button>
+              <LayoutDashboard  className='h-4 w-4 '/>
+              <span className='hidden md:block'>Industry Insights</span>
+            </Button>
+            </Link>
+          </SignedIn>
+        <DropdownMenu>
+        <DropdownMenuTrigger>
+                <Button>
+                  <StarsIcon className='h-4 w-4 '/>
+                  <span className='hidden md:block'>Growth Tools</span>
+                  <ChevronDown className='h-4 w-4 '/>
+                </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuItem>Profile</DropdownMenuItem>
+          <DropdownMenuItem>Billing</DropdownMenuItem>
+          <DropdownMenuItem>Team</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+        </div>
+      </nav>
+            <SignedOut>
+              <SignInButton />
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>      
+    </header>
+  )
+}
+
+export default Header
